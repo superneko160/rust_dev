bae3069c259bbdf259a857a5d5e6ebc3effadcfd
superneko <supernekocat31@gmail.com>
2022-10-16 23:39:04 +0900

    Divide functions

--- n255-analysis/src/main.rs
+++ n255-analysis/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::process;
 use std::path::Path;
 use serde::Deserialize;
 use anyhow::Result;
@@ -25,10 +26,13 @@ fn main() {
     // データの取得
     let data = match read_csv(csv_path) {
         Ok(data) => data,
-        Err(e) => panic!("{}", e),
+        Err(e) => {
+            println!("Error: {}", e);
+            process::exit(1);
+        }
     };
     // データの表示
-    print_data(data);
+    print_data(get_data_mtm_percent_over_stdval(data, 2.0));
 }
 
 /**
@@ -38,11 +42,26 @@ fn read_csv(csv_path: &Path) -> Result<Vec<Data>> {
     let mut data = Vec::new();
     let csv_text = fs::read_to_string(csv_path)?;
     let mut rdr = csv::Reader::from_reader(csv_text.as_bytes());
-    for result in rdr.records() {
-        let record = result?.deserialize(None)?;
-        data.push(record);
+    for record in rdr.records() {
+        let tmp = record?.deserialize(None)?;
+        data.push(tmp);
     }
     Ok(data)
+}
+
+/**
+ * 前月比（％）が基準値（stdval）以上のデータだけを取得
+ */
+fn get_data_mtm_percent_over_stdval(pre_data: Vec<Data>, stdval: f32) -> Vec<Data> {
+    let mut data = Vec::new();
+    for val in pre_data {
+        let mtm_percent: f32 = val.month_to_month_percent.parse().unwrap();
+        // 前月比が基準値以上だった月だけ表示
+        if mtm_percent >= stdval {
+            data.push(val);
+        }
+    }
+    data
 }
 
 /**
@@ -50,10 +69,6 @@ fn read_csv(csv_path: &Path) -> Result<Vec<Data>> {
  */
 fn print_data(data: Vec<Data>) {
     for val in data {
-        // 前月比がプラスだった月だけ表示
-        let mtm_percent: f32 = val.month_to_month_percent.parse().unwrap();
-        if mtm_percent > 0.0 {
-            println!("{} {}", val.date, val.month_to_month_percent);
-        }
+        println!("{}  {}  {}%", val.date, val.month_to_month, val.month_to_month_percent);
     }
 }
