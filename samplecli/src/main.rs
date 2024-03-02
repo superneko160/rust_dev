d8d59ee93866f2d77a16443110e23fd8c7260a8a
sneko <supernekocat31@gmail.com>
2024-03-02 20:41:31 +0900

    modified samplecli drive macro

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,28 +1,25 @@
-use clap::{App, Arg};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,  // コマンドライン引数 1
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,  // コマンドライン引数 2
+}
 
 fn main() {
-    let matches = App::new("My RPN program")
-        .version("1.0.0")
-        .author("Your name")
-        .about("Super awesome sample RPN calculator")
-        .arg(
-            Arg::new("fomula_file")
-            .value_name("FILE")
-            .index(1)
-            .required(false),
-        )
-        .arg(
-            Arg::new("verbose")
-            .short('v')  // charなのでシングルクォート
-            .long("verbose")
-            .required(false),
-        )
-        .get_matches();
-
-    match matches.value_of("fomula_file") {
+    let opts = Opts::parse();
+    match opts.formula_file {
         Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified"),
+        None => println!("No file specified."),
     }
-    let verbose = matches.is_present("verbose");
-    println!("Is verbosity specifed?: {}", verbose);
+    println!("Is verbosity specified?: {}", opts.verbose);
 }
