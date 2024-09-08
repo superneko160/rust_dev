a0583a3b84aa789166a426516d89d708285297c2
sneko <supernekocat31@gmail.com>
2024-09-08 23:24:27 +0900

    feat:add minigrep(The Rust book)

---
+++ minigrep/src/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+use std::process;
+use minigrep::Config;
+
+/// ファイルから特定の文字列を利用している行を抽出
+/// コマンドライン引数で第1引数に対象となる文字、第2引数に対象となるファイルを指定
+/// ex) cargo run frog poem.txt
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(err) = minigrep::run(config) {
+        println!("Application error: {}", err);
+        process::exit(1);
+    }
+}
