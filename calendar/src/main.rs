f5c233e9c85c2c7e081534375821b3795adf9f49
sneko <supernekocat31@gmail.com>
2024-09-15 19:01:35 +0900

    refactor:use ymd() -> with_ymd_and_hms()

--- calendar/src/main.rs
+++ calendar/src/main.rs
@@ -11,14 +11,18 @@ fn main() {
     let now = Tokyo.from_utc_datetime(&utc);
     let (_is_common_era, year) = now.year_ce();
     println!("今月:{}-{:02}-{:02}({:?})", year as i32, now.month(), now.day(), now.weekday());
+
     // 現在の月の1日目の曜日を取得
-    let first_dt = Tokyo.ymd(year as i32, now.month(), 1);
+    let first_dt = Tokyo.with_ymd_and_hms(year as i32, now.month(), 1, 0, 0, 0).unwrap();
     println!("月初:{}-{:02}-{:02}({:?})", year, first_dt.month(), first_dt.day(), first_dt.weekday());
+
     // 月末日を取得（今月の初日から+1ヶ月して-1日した日付を取得）
-    let last_dt = Tokyo.ymd(year as i32, now.month()+1, 1) + Duration::days(-1);
+    let last_dt = Tokyo.with_ymd_and_hms(year as i32, now.month()+1, 1, 0, 0, 0).unwrap() + Duration::days(-1);
     println!("月末:{}-{:02}-{:02}({:?})", year, last_dt.month(), last_dt.day(), last_dt.weekday());
+
     // 曜日
     let weekday: [&str; 7] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
     // 月初日の曜日からスタート地点を決定
     let mut start_p = 0;
     for i in 0..7 {
@@ -27,6 +31,7 @@ fn main() {
             break;
         }
     }
+
     // カレンダ作成
     let mut cal = Vec::new();
     for _i in 0..start_p {
@@ -35,6 +40,7 @@ fn main() {
     for i in 1..last_dt.day()+1 {
         cal.push(i);
     }
+
    // カレンダ表示
     for elm in weekday {
         print!(" {}", elm);
