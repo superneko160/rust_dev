f8620fd16cf1265d61a7166a9613cec124577302
sneko <supernekocat31@gmail.com>
2024-09-21 15:14:44 +0900

    feat:add insertion_sort()

---
+++ sorting/src/insertion_sort.rs
@@ -0,0 +1,38 @@
+/// 挿入ソート（破壊的）
+/// @param &mut [T] list 並び替える要素が格納されたリスト（配列、ベクタ等）
+/// @param bool reverse true:降順, false:昇順
+pub fn insertion_sort<T: Ord + Copy>(list: &mut [T], reverse: bool) {
+    for i in 1..list.len() {
+
+        let mut target_index = i;  // 挿入する箇所
+        let current = list[i];  // 挿入する要素
+
+        while target_index > 0 && (current < list[target_index - 1]) ^ reverse {
+            list[target_index] = list[target_index - 1];
+            target_index -= 1;
+        }
+
+        list[target_index] = current;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_insertion_sort_asc() {
+        let mut list = [2, 7, 3, 5, 1, 24, 31, 100, 11];
+        let result = [1, 2, 3, 5, 7, 11, 24, 31, 100];
+        insertion_sort(&mut list, false);
+        assert_eq!(result, list);
+    }
+
+    #[test]
+    fn test_insertion_sort_desc() {
+        let mut list = [2, 7, 3, 5, 1, 24, 31, 100, 11];
+        let result = [100, 31, 24, 11, 7, 5, 3, 2, 1];
+        insertion_sort(&mut list, true);
+        assert_eq!(result, list);
+    }
+}
--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -1,9 +1,11 @@
-use sorting::bubble_sort::bubble_sort;
+// use sorting::bubble_sort::bubble_sort;
+use sorting::insertion_sort::insertion_sort;
 
 fn main() {
     let mut list = [2, 7, 3, 5, 1, 24, 31, 100, 11];
 
-    bubble_sort(&mut list, true);
+    // bubble_sort(&mut list, true);
+    insertion_sort(&mut list, true);
 
     for val in &list {
         print!("{} ", val);
