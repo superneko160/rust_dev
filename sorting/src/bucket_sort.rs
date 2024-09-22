7bd75b0831684cbcff1ad9d64fae1868093bc5c0
sneko <supernekocat31@gmail.com>
2024-09-22 21:17:34 +0900

    feat:add bucket_sort()

---
+++ sorting/src/bucket_sort.rs
@@ -0,0 +1,64 @@
+use crate::insertion_sort::insertion_sort;
+
+/// バケットソート（バケツソート）
+/// @param &[usize] list 並び替える要素が格納されたリスト
+/// @return Vec<usize> ソート後ベクタ
+pub fn bucket_sort(list: &[usize]) -> Vec<usize> {
+    if list.is_empty() {
+        return vec![];
+    }
+
+    let max = *list.iter().max().unwrap();
+    let length = list.len();
+    let mut buckets = vec![vec![]; length + 1];
+
+    // 要素を適切なバケツに振り分け
+    //「適切」の意味:
+    // 小さい値は小さいインデックスのバケツに
+    // 大きい値は大きいインデックスのバケツに
+    // 値の大きさに比例してバケツが選ばれる
+    // 上の方法により、値の範囲に応じて要素が分散され、その後の各バケツ内でのソートが効率的に行える
+    for x in list {
+        buckets[length * *x / max].push(*x);
+    }
+
+    // デバッグ: 各バケツの中身を表示
+    // println!("Buckets:");
+    // for (i, bucket) in buckets.iter().enumerate() {
+    //     println!("Bucket {}: {:?}", i, bucket);
+    // }
+
+    // 各バケツ内の要素を挿入ソートで並び替え
+    // note:挿入ソート以外のアルゴリズムでも問題ない
+    for bucket in buckets.iter_mut() {
+        insertion_sort(bucket, false);
+    }
+
+    // デバッグ: 各バケツの中身を表示
+    // println!("Buckets(Sorted):");
+    // for (i, bucket) in buckets.iter().enumerate() {
+    //     println!("Bucket {}: {:?}", i, bucket);
+    // }
+
+    // ソートされたバケツを1つの結果リストにマージ
+    let mut result = vec![];
+    for bucket in buckets {
+        for x in bucket {
+            result.push(x);
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bucket_sort_asc() {
+        let mut list = [2, 7, 2, 5, 1, 11, 31, 100, 11];
+        let result = vec![1, 2, 2, 5, 7, 11, 11, 31, 100];
+        assert_eq!(result, bucket_sort(&mut list));
+    }
+}
--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -1,13 +1,16 @@
 // use sorting::bubble_sort::bubble_sort;
 // use sorting::insertion_sort::insertion_sort;
-use sorting::selection_sort::selection_sort;
+// use sorting::selection_sort::selection_sort;
+use sorting::bucket_sort::bucket_sort;
 
 fn main() {
-    let mut list = [2, 7, 3, 5, 1, 24, 31, 100, 11];
+    // let mut list = [2, 7, 3, 5, 1, 24, 31, 100, 11];
+    let list = [2, 7, 2, 5, 1, 11, 31, 100, 11];
 
     // bubble_sort(&mut list, true);
     // insertion_sort(&mut list, true);
-    selection_sort(&mut list, true);
+    // selection_sort(&mut list, true);
+    let list: Vec<usize> = bucket_sort(&list);
 
     for val in &list {
         print!("{} ", val);
