d64793732fbde720b3569f2552187b95d286ac8d
sneko <supernekocat31@gmail.com>
2024-09-22 19:14:14 +0900

    feat:add selection_sort()

--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -1,11 +1,13 @@
 // use sorting::bubble_sort::bubble_sort;
-use sorting::insertion_sort::insertion_sort;
+// use sorting::insertion_sort::insertion_sort;
+use sorting::selection_sort::selection_sort;
 
 fn main() {
     let mut list = [2, 7, 3, 5, 1, 24, 31, 100, 11];
 
     // bubble_sort(&mut list, true);
-    insertion_sort(&mut list, true);
+    // insertion_sort(&mut list, true);
+    selection_sort(&mut list, true);
 
     for val in &list {
         print!("{} ", val);
---
+++ sorting/src/selection_sort.rs
@@ -0,0 +1,41 @@
+/// 選択ソート（破壊的）
+/// @param &mut [T] list 並び替える要素が格納されたリスト（配列、ベクタ等）
+/// @param bool reverse true:降順, false:昇順
+pub fn selection_sort<T: Ord>(list: &mut [T], reverse: bool) {
+
+    let length = list.len();
+
+    for left in 0..length {
+
+        let mut maxmin = left;
+
+        for right in (left + 1)..length {
+            if (list[right] < list[maxmin]) ^ reverse {
+                maxmin = right;
+            }
+        }
+
+        list.swap(maxmin, left);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_selection_sort_asc() {
+        let mut list = [2, 7, 3, 5, 1, 24, 31, 100, 11];
+        let result = [1, 2, 3, 5, 7, 11, 24, 31, 100];
+        selection_sort(&mut list, false);
+        assert_eq!(result, list);
+    }
+
+    #[test]
+    fn test_selection_sort_desc() {
+        let mut list = [2, 7, 3, 5, 1, 24, 31, 100, 11];
+        let result = [100, 31, 24, 11, 7, 5, 3, 2, 1];
+        selection_sort(&mut list, true);
+        assert_eq!(result, list);
+    }
+}
