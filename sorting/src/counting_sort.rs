0fd40c056798394d78c00de39d07bbf60fb5f29a
sneko <supernekocat31@gmail.com>
2024-09-23 19:28:55 +0900

    feat:add counting_sort()

---
+++ sorting/src/counting_sort.rs
@@ -0,0 +1,37 @@
+/// カウンティングソート（破壊的）
+/// 同じ値が複数ある場合に、計算量が少なくなり、処理速度が向上する可能性有
+/// @param &mut [usize] list 並び替える要素が格納されたリスト
+/// @param usize maxval 入力がとり得る最大値（[2, 15, 7]の場合15）
+pub fn counting_sort(list: &mut [usize], maxval: usize) {
+
+    // カウント用ベクタ
+    let mut counter: Vec<usize> = vec![0; maxval + 1];
+
+    // 各要素の出現回数をカウント
+    for &data in list.iter() {
+        counter[data as usize] += 1;
+    }
+
+    // カウント用ベクタを走査し、ソートしたリストに再構築
+    let mut i = 0;
+    for (data, &number) in counter.iter().enumerate() {
+        // 各値について、出現回数分だけリストに追加
+        for _ in 0..number {
+            list[i] = data as usize;
+            i += 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_counting_sort_asc() {
+        let mut list = [2, 7, 2, 5, 1, 11, 31, 100, 11];
+        let result = [1, 2, 2, 5, 7, 11, 11, 31, 100];
+        counting_sort(&mut list, 100);
+        assert_eq!(result, list);
+    }
+}
--- sorting/src/main.rs
+++ sorting/src/main.rs
@@ -1,16 +1,18 @@
 // use sorting::bubble_sort::bubble_sort;
 // use sorting::insertion_sort::insertion_sort;
 // use sorting::selection_sort::selection_sort;
-use sorting::bucket_sort::bucket_sort;
+// use sorting::bucket_sort::bucket_sort;
+use sorting::counting_sort::counting_sort;
 
 fn main() {
-    // let mut list = [2, 7, 3, 5, 1, 24, 31, 100, 11];
-    let list = [2, 7, 2, 5, 1, 11, 31, 100, 11];
-
+    let mut list = [2, 7, 3, 5, 1, 24, 31, 100, 11];
     // bubble_sort(&mut list, true);
     // insertion_sort(&mut list, true);
     // selection_sort(&mut list, true);
-    let list: Vec<usize> = bucket_sort(&list);
+    counting_sort(&mut list, 100);
+
+    // let list = [2, 7, 2, 5, 1, 11, 31, 100, 11];
+    // let list: Vec<usize> = bucket_sort(&list);
 
     for val in &list {
         print!("{} ", val);
