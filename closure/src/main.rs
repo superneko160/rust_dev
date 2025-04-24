2336976fe0df261b852bd20cec2dd237e6cbdbfc
sneko <supernekocat31@gmail.com>
2025-04-24 23:25:28 +0900

    fix: correct comment and add high-order function example

--- closure/src/main.rs
+++ closure/src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
     {
-        // クロージャは多言語でいうところの無名関数やラムダ式に近い
+        // クロージャは他言語でいうところの無名関数やラムダ式に近い
         let square = |x: i32| {
             x * x
         };
         println!("{}", square(3));  // 9
     }
+
     {
         // moveはクロージャ下で参照するクロージャ外変数が存在する場合
         // その所有権をクロージャに移す
@@ -24,5 +25,21 @@ fn main() {
         //     echo $msg;
         // };
         // $func();
+    }
+
+    // 高階関数の例
+    {
+        // クロージャを定義
+        let add_one = |x| x + 1;
+        let multiply_by_2 = |x| x * 2;
+
+        // クロージャを高階関数に渡す
+        let result1 = apply_function(add_one, 5);  // 6
+        let result2 = apply_function(multiply_by_2, 5);  // 10
+        println!("{} {}", result1, result2);
+        
+        fn apply_function<F>(f: F, value: i32) -> i32 where F: Fn(i32) -> i32 {
+            f(value)
+        }
     }
 }
