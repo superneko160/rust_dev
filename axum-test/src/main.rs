e2a3ffd9cdeba75ec6c7eabf382ba765a49912bb
sneko <supernekocat31@gmail.com>
2025-11-03 21:47:22 +0900

    feat: use validator(axum-test)

--- axum-test/src/main.rs
+++ axum-test/src/main.rs
@@ -1,13 +1,13 @@
 use axum::{
     extract::{Form, Path, Query},
     http::StatusCode,
-    response::Html,
-    response::IntoResponse,
+    response::{Html, IntoResponse, Response},
     routing::{get, post},
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use validator::Validate;
 
 #[derive(Deserialize, Serialize)]
 struct Pagination {
@@ -26,9 +26,12 @@ struct UserWithoutId {
     name: String,
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Validate)]
 struct FormInput {
+    #[validate(length(min = 1, message = "名前を入力してください"))]
     name: String,
+    #[validate(length(min = 1, message = "メールアドレスを入力してください"))]
+    #[validate(email(message = "正しいメールアドレスの形式で入力してください"))]
     email: String,
 }
 
@@ -111,8 +114,68 @@ async fn contact() -> Html<String> {
     Html(contents)
 }
 
-async fn accept_contact(Form(input): Form<FormInput>) -> Html<String> {
-    Html(format!("name: {} (email: {})", &input.name, &input.email))
+async fn accept_contact(Form(input): Form<FormInput>) -> impl IntoResponse {
+    match input.validate() {
+        Ok(_) => create_success_response(&input),
+        Err(errors) => create_error_response(errors),
+    }
+}
+
+/// バリデーション成功時のレスポンスを生成する
+/// 入力された名前とメールアドレスを表示するHTMLを返す
+fn create_success_response(input: &FormInput) -> Response {
+    Html(format!("name: {} (email: {})", &input.name, &input.email)).into_response()
+}
+
+/// バリデーション失敗時のエラーレスポンスを生成する
+/// エラーメッセージを一覧表示するHTMLと400ステータスコードを返す
+fn create_error_response(errors: validator::ValidationErrors) -> Response {
+    // バリデーションエラーからエラーメッセージの配列を抽出
+    let error_messages = extract_error_messages(errors);
+
+    let error_html = format!(
+        r#"
+        <!doctype html>
+        <html>
+        <head>
+            <title>バリデーションエラー</title>
+        </head>
+        <body>
+            <h1>エラーが発生しました</h1>
+            <ul>
+                {}
+            </ul>
+            <a href="/contact">戻る</a>
+        </body>
+        </html>
+        "#,
+        error_messages
+            .iter()
+            .map(|msg| format!("<li>{}</li>", msg))
+            .collect::<Vec<_>>()
+            .join("\n")
+    );
+
+    (StatusCode::BAD_REQUEST, Html(error_html)).into_response()
+}
+
+/// バリデーションエラーからエラーメッセージを抽出する
+/// 各フィールドのエラー情報を走査し、メッセージを収集する
+fn extract_error_messages(errors: validator::ValidationErrors) -> Vec<String> {
+    errors
+        .field_errors()
+        .iter()
+        .flat_map(|(field, errors)| {
+            errors.iter().map(move |error| {
+                // カスタムメッセージがあればそれを使用、なければデフォルトメッセージを利用
+                error
+                    .message
+                    .as_ref()
+                    .map(|m| m.to_string())
+                    .unwrap_or_else(|| format!("{}のバリデーションエラー", field))
+            })
+        })
+        .collect()
 }
 
 async fn handler_404() -> impl IntoResponse {
@@ -122,10 +185,7 @@ async fn handler_404() -> impl IntoResponse {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use axum::{
-        body::Body,
-        http::{Request, StatusCode},
-    };
+    use axum::{body::Body, http::Request};
     use tower::util::ServiceExt;
 
     #[tokio::test]
